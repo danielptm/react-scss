91a86adf66f49a0b42b00791f951edf14d6cd6c1
Daniel <danielptm@me.com>
2018-08-08 22:17:57 +0200

    Fixed som div bug issues

--- src/App/routes/About/About.js
+++ src/App/routes/About/About.js
@@ -58,26 +58,25 @@ class About extends Component {
 
     render(){
         return (
-            <div className={style.container}>
-                <div className={style.mainImageContainer}>
-                    <img className={style.image} src={image} />
-                    <button
-                        disabled
-                        onClick={this.routeToSlideShow}
-                        className={style.imagesButton}>
-                        <div className={style.toolTip}>Image slide show coming soon!</div>
-                        <img className={style.images} src={images} /></button>
-                </div>
-                <div className={style.about}>
-                    {this.state.english ? this.englishText() : this.swedishText() }
-                    <div className={style.flagContainer}>
-                        <img onClick={this.toggleEnglish} className={style.flag} src={usa} />
-                        <img onClick={this.toggleSwedish} className={style.flag} src={sweden} />
-                    </div>
-                    <div>
-                    </div>
-                </div>
+        <div className={style.container}>
+            <div className={style.mainImageContainer}>
+                <img className={style.image} src={image} />
+                <button
+                    disabled
+                    onClick={this.routeToSlideShow}
+                    className={style.imagesButton}>
+                    <div className={style.toolTip}>Image slide show coming soon!</div>
+                  <img className={style.images} src={images} />
+                </button>
             </div>
+            <div className={style.about}>
+                {this.state.english ? this.englishText() : this.swedishText() }
+            </div>
+            <div className={style.flagContainer}>
+                <img onClick={this.toggleEnglish} className={style.flag} src={usa} />
+                <img onClick={this.toggleSwedish} className={style.flag} src={sweden} />
+            </div>
+        </div>
         )
     }
 }
